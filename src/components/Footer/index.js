69e4a12a0eee7da3e3c593e2d2b5427906239f7a
Ambika Kochuparambil Sidharthan <ak21ace@herts.ac.uk>
2023-02-22 23:08:13 +0000

    Added Dots Animation

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -15,19 +15,22 @@ const Footer=() =>{
 <div className="container-fluid">
   <div className="footer">
     <div className="row">
-        <div className="col-md-3">
+        <div className="col-md-4">
             {/* <h4>Wallet Saver</h4> */}
             <img src={iconImage} className="iconImage" alt='logo'/>
             <p className='logo-text'>Save More .. Shop More</p>
         </div>
 
-        <div className="col-md-3">
-            <h5><b>About Us</b></h5>
+        {/* <div className="col-md-3">
+            <h5>About Us<div className="textUnderline"><span></span></div></h5>
             <p>some text..</p>
-        </div>
+        </div> */}
 
-        <div className="col-md-3">
-            <h5><b>Contact Us</b></h5>
+        <div className="col-md-4">
+            <h5><b>Contact Us<div className="textUnderline"><span></span></div>
+            <div className="textUnderline"><span></span></div>
+            <div className="textUnderline"><span></span></div>
+            </b></h5>
                 <ul className="list-unstyled">
                     <li><FontAwesomeIcon icon={faHome}/>&emsp;Edx Bootcamp,&nbsp;</li>
                     <li>&emsp;&emsp;&emsp;United Kingdom</li>
@@ -35,8 +38,11 @@ const Footer=() =>{
                 </ul>
         </div>
 
-        <div className="col-md-3 social-media-icons ">
-            <h5><b>Follow Us</b></h5>
+        <div className="col-md-4 social-media-icons ">
+            <h5><b>Follow Us<div className="textUnderline"><span></span></div>
+            <div className="textUnderline"><span></span></div>
+            <div className="textUnderline"><span></span></div>
+            </b></h5>
             <div>
             <a href='https://facebook.com'>
                 <FontAwesomeIcon icon={faFacebook}/>
