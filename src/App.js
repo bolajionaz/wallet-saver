346127e6770b325da2d094d5f849036eff5f3dd8
bolajionaz <bolajionaz@gmail.com>
2023-02-22 22:41:37 +0000

    navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import Footer from './components/Footer';
+import Nav from './components/Nav';
 
 function App() {
   return (
     <div className="App">
+         <Nav />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
