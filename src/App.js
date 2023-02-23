9bd19b2c75cc5ff6d78acb521a7a32e7acc42e1f
ezechukwu nwokedi <israellambert96@gmail.com>
2023-02-23 11:51:09 +0000

    added icons and individual store page

--- src/App.js
+++ src/App.js
@@ -1,30 +1,32 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
-import HomePage from './pages/HomePage';
+
 import Footer from './components/Footer';
 import Nav from './components/Nav';
+
+import HomePage from './pages/HomePage';
+import ChooseStore from './pages/StoreChoice';
 
 function App() {
   return (
     <div  className='App'>
-    <Router>
-    <Nav />
-    {/* Wrap Route elements in a Routes component */}
-    <Routes>
-      {/* Define routes using the Route component to render different page components at different paths */}
-      {/* Define a default route that will render the Home component */}
-      <Route path="/" element={<HomePage />} />
-      {/* <Route path="/search/stores" element={<ChooseStore />} />
-      <Route path="/search/stores/aliExpress" element={<SearchOnlineStores searchOpt="aliExpress" />} />
-      <Route path="/search/stores/amazon" element={<SearchOnlineStores searchOpt="amazon" />} />
-      <Route path="/search/stores/compare" element={<SearchOnlineStores searchOpt="compare" /> } />
-      <Route path='/about' element={<About/>}></Route>
-      <Route path='/faq' element={<FAQs/>}></Route> */}
-      {/* Define a route that will have descendant routes */}
-      {/* <Route path="contact/*" element={<Contact />} /> */}
-    </Routes>
-    <Footer />
-</Router>
+      <Router>
+      <Nav />
+      {/* Wrap Route elements in a Routes component */}
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/search/stores" element={<ChooseStore />} />
+        {/* 
+        <Route path="/search/stores/aliExpress" element={<SearchOnlineStores searchOpt="aliExpress" />} />
+        <Route path="/search/stores/amazon" element={<SearchOnlineStores searchOpt="amazon" />} />
+        <Route path="/search/stores/compare" element={<SearchOnlineStores searchOpt="compare" /> } />
+        <Route path='/about' element={<About/>}></Route>
+        <Route path='/faq' element={<FAQs/>}></Route> */}
+        {/* Define a route that will have descendant routes */}
+        {/* <Route path="contact/*" element={<Contact />} /> */}
+      </Routes>
+      <Footer />
+  </Router>
 </div>
   );
 }
