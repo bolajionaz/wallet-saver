51db80f45e63f1456269b5c515aa63083b7b39d0
bolajionaz <bolajionaz@gmail.com>
2023-02-23 01:27:57 +0000

    new chnag

--- src/pages/HomePage/index.js
+++ src/pages/HomePage/index.js
@@ -4,7 +4,7 @@ import './index.css';
 function HomePage() {
     return (<div className="container">
         <img src="/pictures/pic1.jpg" alt="HomePage" className="HomeImage" />
-        <div className="Slogan">
+        <div className="Slogan ">
             Everything, Everywhere, <br /> All at once ...
 
         </div>
