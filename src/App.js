9ae45a9ca201234c81dca2821508c768e46c3b83
ezechukwu nwokedi <israellambert96@gmail.com>
2023-02-23 10:41:42 +0000

    react-dom implemented

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
 import HomePage from './pages/HomePage';
 import Footer from './components/Footer';
@@ -6,13 +6,24 @@ import Nav from './components/Nav';
 
 function App() {
   return (
-    <div className="App">
-         <Nav />
-      <header className="App-header">
-        <HomePage />
-      </header>
-      <Footer />
-    </div>
+    <Router className="App">
+    <Nav />
+    {/* Wrap Route elements in a Routes component */}
+    <Routes>
+      {/* Define routes using the Route component to render different page components at different paths */}
+      {/* Define a default route that will render the Home component */}
+      <Route path="/" element={<HomePage />} />
+      {/* <Route path="/search/stores" element={<ChooseStore />} />
+      <Route path="/search/stores/aliExpress" element={<SearchOnlineStores searchOpt="aliExpress" />} />
+      <Route path="/search/stores/amazon" element={<SearchOnlineStores searchOpt="amazon" />} />
+      <Route path="/search/stores/compare" element={<SearchOnlineStores searchOpt="compare" /> } />
+      <Route path='/about' element={<About/>}></Route>
+      <Route path='/faq' element={<FAQs/>}></Route> */}
+      {/* Define a route that will have descendant routes */}
+      {/* <Route path="contact/*" element={<Contact />} /> */}
+    </Routes>
+    <Footer />
+</Router>
   );
 }
 
--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,18 +1,56 @@
 import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './nav.css';
+import { NavLink } from "react-router-dom";
+import './index.css';
 
-const Nav=() =>{
-    return (
-      <div className='d-flex menu-container' id='bar'>
+{/*<div className='d-flex menu-container' id='bar'>
           <a className='me-auto p-2 navbar-brand'><img src="/pictures/icon2.png"/><strong>Wallet Saver</strong></a>
         
             <ul className='d-flex'>
           <li className='right-menu p-2'><a>Compare Stores</a></li>
           <li className='right-menu p-2'><a>Individual Stores</a></li>
         </ul>
+      </div>*/}
+
+function Nav () {
+    return (
+      
+      <nav className="navbar navbar-expand-md navbar-light" id='bar'>
+        <div className="container-fluid ">
+        {/* <NavLink to="/" className="m-1 mt-0 mb-0">
+            <img src="/WalletSavers.png" width="40" height="40" 
+                    className="d-inline-block align-center" alt="navigation-bar" />
+        </NavLink> */}
+        <NavLink to="/" className="me-auto p-0 navbar-brand">
+          <img src="/pictures/icon2.png"/><strong>Wallet Saver</strong>
+        </NavLink>
+        
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+        </button>
+        
+        <div className="collapse navbar-collapse justify-content-end" id="navbarNav">
+            <ul className="navbar-nav mb-lg-0">
+                <li className="nav-item"> 
+                    <NavLink
+                      to="search/stores"
+                      className={({ isActive }) =>
+                        isActive ? 'right-menu nav-link active' : 'right-menu nav-link'}>
+                      Individual Stores
+                    </NavLink>
+                </li>
+                <li className="nav-item"> 
+                    <NavLink
+                        to="search/stores/compare"
+                        className={({ isActive }) =>
+                        isActive ? 'right-menu nav-link active' : 'right-menu nav-link'}>
+                        Compare Stores
+                    </NavLink>
+                </li>
+            </ul>
+          </div>
         </div>
-  
+        
+      </nav>
     );
 
 }
