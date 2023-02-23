ae1c7b0517ef6fc578db92c8ca73e56113541eb4
bolajionaz <bolajionaz@gmail.com>
2023-02-23 00:07:59 +0000

    Icon imgae moved to pictures folder

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -2,11 +2,10 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './nav.css';
 
-import iconImage from './icon2.png'
 const Nav=() =>{
     return (
       <div className='d-flex menu-container' id='bar'>
-          <a className='me-auto p-2 navbar-brand'><img src={iconImage}/><strong>Wallet Saver</strong></a>
+          <a className='me-auto p-2 navbar-brand'><img src="/pictures/icon2.png"/><strong>Wallet Saver</strong></a>
         
             <ul className='d-flex'>
           <li className='right-menu p-2'><a>Compare Stores</a></li>
